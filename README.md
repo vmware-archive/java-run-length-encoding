d8b4e67d86dc57d66aa90a5d35dd258175953d74
Glyn Normington <gnormington@pivotal.io>
2018-02-21 12:47:00 +0000

    Improve README and test edge case

--- README.md
+++ README.md
@@ -5,5 +5,29 @@ This sample demonstrates functions of finite streams that maintain stream-relate
 The `Encode` function takes an input stream of integers and emits an output stream
 after applying [run-length encoding](https://en.wikipedia.org/wiki/Run-length_encoding).
 Encoding or decoding an empty input stream results in an empty output stream.
+For example, the stream:
+```
+< 0, 1, 1, 2, 2, 2 >
+
+```
+encodes to the following stream:
+```
+< 1, 0, 2, 1, 3, 2 >
+
+```
+
 The `Decode` function takes a run-length encoded input stream and emits a decoded output stream.
-`Encode` followed by `Decode` produces the original stream of integers. 
+For example, the stream:
+```
+< 3, 1, 1, 0 >
+
+```
+decodes to the following stream:
+```
+< 1, 1, 1, 0 >
+
+```
+
+Of course, `Encode` followed by `Decode` produces the original stream of integers.
+
+**Optional exercise:** when will `Decode` followed by `Encode` _not_ produce the original stream of integers? 
--- decode/src/test/java/functions/DecodeTests.java
+++ decode/src/test/java/functions/DecodeTests.java
@@ -37,6 +37,20 @@ public class DecodeTests {
 	}
 
 	@Test
+	public void testDegenerateRun() {
+		StepVerifier.create(Flux.just(1, 0, 0, 1, 1, 2).as(decode))
+				.expectNext(0, 2)
+				.verifyComplete();
+	}
+
+	@Test
+	public void testUnexpectedRun() {
+		StepVerifier.create(Flux.just(1, 0, 1, 0).as(decode))
+				.expectNext(0, 0)
+				.verifyComplete();
+	}
+
+	@Test
 	public void testLong() {
 		StepVerifier.create(Flux.just(1000, 0).as(decode))
 				.expectNextSequence(Collections.nCopies(1000, 0))
@@ -44,8 +58,16 @@ public class DecodeTests {
 	}
 
 	@Test
-	public void testInvalid() {
-		StepVerifier.create(Flux.just(0).as(decode)) // Count but no value
+	public void testCountWithNoValue() {
+		StepVerifier.create(Flux.just(2, 1, 0).as(decode))
+				.expectNext(1, 1)
+				.verifyError(IllegalArgumentException.class);
+	}
+
+	@Test
+	public void testNegativeCount() {
+		StepVerifier.create(Flux.just(2, 1, -1, 0, 2, 2).as(decode))
+				.expectNext(1, 1)
 				.verifyError(IllegalArgumentException.class);
 	}
 }
